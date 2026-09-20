100ee4ab10c16aa5b9477682a16cf153bb24bd07
Nicholas <nicholas.mob1010@gmail.com>
2026-09-20 20:52:34 -0300

    Update script.js

---
+++ static/script.js
@@ -0,0 +1,16 @@
+const audio = document.getElementById("audio");
+const legenda = document.getElementById("legenda");
+
+const track = audio.textTracks[0];
+
+track.mode = "hidden";
+
+track.addEventListener("cuechange", () => {
+    const cue = track.activeCues[0];
+
+    if (cue) {
+        legenda.textContent = cue.text;
+    } else {
+        legenda.textContent = "";
+    }
+});
